2774b5f0364ef735df1aa7762940499496be84fc
dags- <dags@dags.me>
2018-04-24 09:25:30 +0100

    use locally stored quotes

--- main.go
+++ main.go
@@ -6,44 +6,38 @@ import (
 	"os"
 
 	"github.com/bwmarrin/discordgo"
-	"github.com/dags-/CaptainEggplant/quotes"
 	"bufio"
+	"github.com/dags-/CaptainEggplant/quotes"
+	"math/rand"
 )
 
 // rub: 233182426711588864
 // invite: https://discordapp.com/api/oauth2/authorize?client_id=437870761781231617&permissions=2112&scope=bot
 
 var target *string
-var q *quotes.Quotes
+var q = quotes.New()
 
 func main() {
-	// discord bot api token
 	token := flag.String("token", "", "Auth token")
-	// tumblr api key
-	key := flag.String("key", "", "Tumblr api key")
-	// discord user id
 	target = flag.String("target", "", "Egg-plantee's id")
 	flag.Parse()
 
 	// check all flags provided
-	if *token == "" || *target == "" || *key == "" {
-		fmt.Printf("flags error: token='%s', key='%s', target='%s'\n", *token, *key, *target)
+	if *token == "" || *target == "" {
+		fmt.Printf("flags error: token='%s', target='%s'\n", *token, *target)
 		return
 	}
 
-	// init quotes & discord client
-	q = quotes.New(*key)
+	// init discord client
 	s, e := discordgo.New("Bot " + *token)
 	if e != nil {
 		fmt.Println("login err:", e)
 		return
 	}
 
-	// add event handlers
 	s.AddHandler(join)
 	s.AddHandler(message)
 
-	// open connection
 	e = s.Open()
 	if e != nil {
 		fmt.Println("could not open session:", e)
@@ -61,6 +55,7 @@ func main() {
 
 func handleStop() {
 	scanner := bufio.NewScanner(os.Stdin)
+
 	for scanner.Scan() {
 		if scanner.Text() == "stop" {
 			fmt.Println("Stopping...")
@@ -71,41 +66,66 @@ func handleStop() {
 }
 
 func join(s *discordgo.Session, g *discordgo.GuildCreate) {
+	s.GuildMemberNickname(g.ID, s.State.User.ID, "Captain Eggplant 🍆")
 	fmt.Println("joined guild:", g.Name)
 }
 
 func message(s *discordgo.Session, m *discordgo.MessageCreate) {
-	// message author is our target
+	if m.Author.Bot || m.Author.ID == s.State.User.ID {
+		return
+	}
+
+	// add reaction to target
 	if m.Author.ID == *target {
-		// eggplant that mofo
 		e := s.MessageReactionAdd(m.ChannelID, m.Message.ID, "🍆")
 		if e != nil {
 			fmt.Println("add reaction error:", e)
 		}
+	}
 
-		// automatically reply to user if haven't done so in the last 30 mins
-		respond := q.ShouldRespond()
-		if !respond {
-			// otherwise if target has mentioned the bot, respond directly
-			for _, u := range m.Mentions {
-				if u.ID == s.State.User.ID {
-					respond = true
-					break
-				}
-			}
+	// @mentioning the bot invokes a response otherwise randomly send a message (less frequently)
+	if mentions(s.State.User.ID, m) {
+		// rate limited to once every 15 secs
+		if !q.CanInvoke() {
+			return
 		}
 
-		if respond {
-			// poll the next quote
-			txt := q.NextQuote()
-			if txt != "" {
-				// Oh (mention), with your face like (text)
-				content := fmt.Sprint("Oh ", m.Author.Mention(), ", with your face like ", txt, " :eggplant:")
-				_, e = s.ChannelMessageSend(m.ChannelID, content)
-				if e != nil {
-					fmt.Println(e)
-				}
-			}
+		sendResponse(s, m, q.NextResponse())
+	} else {
+		// rate limited to once every 12 hours
+		if !q.CanRespond() {
+			return
+		}
+
+		// 5% chance of sending a message
+		if rand.Intn(100) < 5 {
+			sendResponse(s, m, q.NextResponse())
+		} else {
+			// set timestamp and try again in 12 hours
+			q.Cooldown()
 		}
 	}
+}
+
+func sendResponse(s *discordgo.Session, m *discordgo.MessageCreate, msg string) {
+	if msg == "" {
+		fmt.Println("send empty message?")
+		return
+	}
+
+	content := fmt.Sprint("Oh ", m.Author.Mention(), ", with your face like a ", msg, " :eggplant:")
+	_, e := s.ChannelMessageSend(m.ChannelID, content)
+
+	if e != nil {
+		fmt.Println("send response error:", e)
+	}
+}
+
+func mentions(id string, m *discordgo.MessageCreate) bool {
+	for _, a := range m.Mentions {
+		if a.ID == id {
+			return true
+		}
+	}
+	return false
 }
--- quotes/quotes.go
+++ quotes/quotes.go
@@ -1,45 +1,63 @@
 package quotes
 
 import (
+	"sync"
+	"time"
+	"io/ioutil"
 	"fmt"
-	"net/http"
 	"encoding/json"
 	"math/rand"
-	"strings"
-	"html"
-	"time"
-	"sync"
 )
 
 type Quotes struct {
 	lock   sync.RWMutex
-	apiKey string
-	last   time.Time
+	invoke time.Time
+	auto   time.Time
 	queue  []string
 }
 
-func New(key string) *Quotes {
+func New() *Quotes {
 	return &Quotes{
-		apiKey: key,
-		last:   time.Now(),
+		auto:   time.Now(),
+		invoke: time.Now(),
 	}
 }
 
-func (q *Quotes) ShouldRespond() bool {
-	// read only
+func (q *Quotes) CanInvoke() bool {
 	q.lock.RLock()
 	defer q.lock.RUnlock()
-
-	// check if 30 mins has passed since last message
-	return time.Since(q.last) > time.Duration(30 * time.Minute)
+	return time.Since(q.invoke) > time.Duration(10*time.Second)
 }
 
-func (q *Quotes) NextQuote() string {
-	// full lock since we may be writing to the queue and/or timestamp
+func (q *Quotes) CanRespond() bool {
+	q.lock.RLock()
+	defer q.lock.RUnlock()
+	return time.Since(q.auto) > time.Duration(12*time.Hour)
+}
+
+func (q *Quotes) Cooldown()  {
 	q.lock.Lock()
 	defer q.lock.Unlock()
+	q.auto = time.Now()
+}
 
-	// fill queue if empty
+func (q *Quotes) NextInvoke() string {
+	q.lock.Lock()
+	defer q.lock.Unlock()
+	quote := next(q)
+	q.invoke = time.Now()
+	return quote
+}
+
+func (q *Quotes) NextResponse() string {
+	q.lock.Lock()
+	defer q.lock.Unlock()
+	quote := next(q)
+	q.auto = time.Now()
+	return quote
+}
+
+func next(q *Quotes) string {
 	if len(q.queue) == 0 {
 		fillQueue(q)
 		// still empty ? :(
@@ -47,44 +65,22 @@ func (q *Quotes) NextQuote() string {
 			return ""
 		}
 	}
-
-	// drain the first quote from the queue
 	quote := q.queue[0]
 	q.queue = q.queue[1:]
-
-	// stamp the last usage time
-	q.last = time.Now()
-
 	return quote
 }
 
 func fillQueue(q *Quotes) {
-	// map post id against the post to filter duplicates posts returned by the api pagination
-	posts := make(map[int]Post)
-	before := ""
-
-	// api responds with 20 items per page
-	// use the timestamp of the 20th item as the 'before' value in the next query
-	for {
-		page := getPage(q.apiKey, before)
-		before = ""
-
-		for _, p := range page {
-			if _, ok := posts[p.ID]; !ok {
-				posts[p.ID] = p
-				before = fmt.Sprint(p.ID)
-			}
-		}
-
-		if before == "" {
-			break
-		}
+	d, e := ioutil.ReadFile("data.json")
+	if e != nil {
+		fmt.Println("read error:", e)
+		return
 	}
 
-	// transfer the unique quotes to the quotes queue
-	q.queue = make([]string, len(posts))
-	for i, p := range posts {
-		q.queue[i] = tidy(p.Text)
+	e = json.Unmarshal(d, &q.queue)
+	if e != nil {
+		fmt.Println("unmarshal error:", e)
+		return
 	}
 
 	// shuffle the queue
@@ -92,10 +88,4 @@ func fillQueue(q *Quotes) {
 		j := rand.Intn(i + 1)
 		q.queue[i], q.queue[j] = q.queue[j], q.queue[i]
 	}
-}
-
-// removes whitespace prefix/suffix and ensures first word is un-capitalized
-func tidy(s string) string {
-	s = strings.Trim(s, " ")
-	return html.UnescapeString(strings.ToLower(string(s[0])) + string(s[1:]))
 }
